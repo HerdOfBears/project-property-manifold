bf294a12aa690c2413054072de85b3abdac4a69b
HerdOfBears <jyler.menard@gmail.com>
2023-08-01 11:38:54 -0400

    bash file for running on cluster

---
+++ mybash.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --account=def-rmansbac
+#SBATCH --time=0-00:05:00
+#SBATCH --job-name="test-run"
+#SBATCH --nodes=1
+
+module load python/3.10 scipy-stack
+source #PROJECTSRRG/project-property-manifold/venv/bin/activate
+
+pip install --no-index --upgrade pip
+pip install --no-index -r requirements.txt
+
+python --version
