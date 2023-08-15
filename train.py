fda6c6e53ab159bbee776282417212ddb1e9800d
HerdOfBears <jyler.menard@gmail.com>
2023-08-15 14:41:10 -0400

    print inputted args

--- train.py
+++ train.py
@@ -119,6 +119,8 @@ if __name__=="__main__":
     else:
         CHKPT_FREQ = N_EPOCHS
     print(f"n_epochs: {N_EPOCHS}, batch_size: {BATCH_SIZE}, lr: {LR}")
+    print(f"chkpt_dir: {CHKPT_DIR}, chkpt_freq: {CHKPT_FREQ}")
+    print(f"n_latent: {N_LATENT}, n_embd: {N_EMBD}, n_hidden_prop: {N_HIDDEN_PROP}")
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     torch.manual_seed(42)
