9569b47a784b8c5986b947c22cdfcdc9c295f85a
HerdOfBears <jyler.menard@gmail.com>
2023-08-16 16:53:21 -0400

    test larger rnn vae

--- mybash.sh
+++ mybash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=rrg-rmansbac
-#SBATCH --time=0-00:05:00
-#SBATCH --job-name="test-run"
+#SBATCH --time=0-00:30:00
+#SBATCH --job-name="test-rnn-vae"
 #SBATCH --nodes=1
 #SBATCH --mem=4G
 #SBATCH --gpus-per-node=1
@@ -13,4 +13,4 @@ pip install --no-index --upgrade pip
 pip install --no-index -r requirements.txt
 
 python --version
-python train.py
+python train.py --batch_size 128 --epochs 10 --n_latent 32 --n_model 64 
