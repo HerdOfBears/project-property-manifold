96aa17acb8a5579d34493bf9ea62f6313032b1de
jyler <jyler.menard@mail.concordia.ca>
2023-09-07 17:03:40 -0400

    different save dir

--- semi-supervised-job.sh
+++ semi-supervised-job.sh
@@ -15,4 +15,4 @@ python train.py \
     --n_model 32 \
     --n_embd 32 \
     --drop_percent_of_labels {$1} \
-    --save_dir "./semi-supervised/drop-{$1}percent/"
+    --save_dir "./experiments/semi-supervised/drop-{$1}percent/"
